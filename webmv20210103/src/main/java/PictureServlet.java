b28fec68ea2256d5da3b70e6ff160af964d3a316
chang1963 <tingchenchang1963@outlook.com>
2021-01-03 16:25:36 +0800

    JPA jQuery

---
+++ webmv20210103/src/main/java/PictureServlet.java
@@ -0,0 +1,44 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class PictureServlet
+ */
+@WebServlet("/PictureServlet")
+public class PictureServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public PictureServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String cnt=request.getParameter("id");
+		String[]  images= {"images/cups.jpg","images/earphone.jpg","images/ipad.jpg","images/lenovo.jpg","images/shoes.jpg"};
+		int count=Integer.parseInt(cnt)%images.length;        		
+		response.getWriter().append(images[count]);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ webmv20210103/src/main/java/model/Student.java
@@ -0,0 +1,54 @@
+package model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the student database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Student.findAll", query="SELECT s FROM Student s")
+public class Student implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private int sid;
+
+	private int age;
+	
+	private String sname;
+	
+
+	public Student() {
+	}
+    public int getSid() {
+    	return sid;
+    }
+	public void setSid(int sid) {
+		this.sid = sid;
+	}
+
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getAge() {
+		return this.age;
+	}
+
+	
+
+	public String getSname() {
+		return this.sname;
+	}
+
+	public void setSname(String name) {
+		this.sname = name;
+	}
+
+	
+
+}
